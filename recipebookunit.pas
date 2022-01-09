451337132e82632620756b5bc8b5c7f63c00993a
earentir <earentir@gmail.com>
2022-01-09 19:19:00 +0200

    moved parsers to their own unit

--- recipebookunit.pas
+++ recipebookunit.pas
@@ -64,7 +64,7 @@ var
 
 implementation
 
-uses recipeparserunit;
+uses recipeparserunit, bitmapmanipulation;
 
 {$R *.lfm}
 
@@ -74,101 +74,6 @@ type
 { TForm1 }
 
 //Icon Attribution: Entypo pictograms by Daniel Bruce — www.entypo.com
-
-//ToDo: Convert to JSON and Store
-//Our Parse (name, length, author, about, address, cost)
-//Parse manual (name, title, length, (4 fields),author, about, address)
-procedure parseRecipe(recipeFile: string; Grid: TstringGrid);
-var
-  csv: TStringGrid;
-  recipeItem: array of string;
-  GUID: TGuid;
-  i, j: integer;
-begin
-  if FileExists(recipeFile) then
-  begin
-    csv := TStringGrid.Create(nil);
-    try
-      csv.LoadFromCSVFile(recipeFile, ',', True, 0, True);
-      if csv.RowCount > 0 then
-      begin
-        insert(recipefile, recipeItem, Length(recipeItem));
-        CreateGUID(GUID); //ToDo: Check if we create proper GUID (if 0> err)
-        insert(GUIDToString(GUID), recipeItem, Length(recipeItem));
-        for j := 0 to Grid.ColCount - 1 do
-        begin
-          for i := 0 to csv.ColCount - 1 do
-          begin
-            if Grid.Columns.Items[j].Title.Caption = csv.Cells[i, 0] then
-            begin
-              insert(csv.Cells[i, 1], recipeItem, Length(recipeItem));
-            end;
-          end;
-        end;
-      end;
-      Grid.InsertRowWithValues(Grid.RowCount, recipeItem);
-    finally
-      csv.Free;
-    end;
-  end;
-end;
-
-procedure gridColResize(colIndex: integer; Grid: TStringGrid);
-var
-  i: integer;
-  minWidth: integer = 0;
-begin
-  for i := 0 to Grid.ColCount - 1 do
-  begin
-    if i <> colIndex then
-      minWidth += Grid.ColWidths[i];
-  end;
-
-  Grid.ColWidths[colIndex] := Grid.Width - minWidth;
-end;
-
-procedure parseIngredients(recipeFile: string; Grid: TstringGrid);
-var
-  csv: TStringGrid;
-  recipeItem: array of string;
-  GUID: TGuid;
-  i, j: integer;
-  ingstr: string = '[ingredients]';
-  partstr: string;
-begin
-  if FileExists(recipeFile) then
-  begin
-    csv := TStringGrid.Create(nil);
-    try
-      csv.LoadFromCSVFile(recipeFile, ',', True, 0, True);
-
-      if csv.RowCount > 0 then
-      begin
-        grid.Columns.Clear;
-
-        partstr := '';
-        //Get Ingredients and build grid
-        for i := 0 to csv.RowCount - 1 do
-        begin
-          if csv.Cells[0, i].Substring(0, Length(ingstr)) = ingstr then
-          begin
-            partstr := csv.Cells[0, i].Substring(length(ingstr), length(csv.Cells[0, i]) - Length(ingstr)).Replace('{', '').Replace('}', '');
-          end;
-
-          if (partstr <> '') and (csv.Cells[0, i].Substring(0, 1) <> '[') then
-          begin
-            if csv.Cells[0, i].Substring(0, Length(ingstr)) <> ingstr then
-              grid.InsertRowWithValues(grid.RowCount, [partstr, csv.Cells[0, i], csv.Cells[2, i], csv.Cells[3, i]]);
-          end;
-        end;
-        gridColResize(1, Grid);
-      end;
-    finally
-      csv.Free;
-    end;
-  end;
-end;
-
 
 procedure LoadRecipes(Grid: TStringGrid);
 var
@@ -191,6 +96,8 @@ begin
 
   //playing with image
   ImagePreview.Picture.LoadFromFile('img/recipe.jpeg');
+
+  //writeln(bitmaptobase64('img/recipe.jpeg'));
 end;
 
 procedure TForm1.SpkToolbar1TabChanged(Sender: TObject);
